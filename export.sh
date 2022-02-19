da19e8ced14c6a453cc2dab9b2c0af57099e9ef8
MagicNeko(Dev:Desktop) <icecat@koishi.moe>
2022-02-19 22:17:09 +0800

    第二遍提交，导出工具改动

--- export.sh
+++ export.sh
@@ -1,6 +1,9 @@
+rm /etc/unbound/unbound.conf.d/100-cnop.conf
+rm /etc/unbound/unbound.conf.d/101-intiop.conf
+rm /etc/unbound/unbound.conf.d/102-mnekoop.conf
+rm /etc/unbound/unbound.conf.d/103-intop.conf
 
 cat conf.cn/*.conf > /etc/unbound/unbound.conf.d/100-cnop.conf
 cat conf.inti/*.conf > /etc/unbound/unbound.conf.d/101-intiop.conf
-cat magicneko/*.conf > /etc/unbound/unbound.conf.d/101-intiop.conf
-cat conf.op/*.conf > /etc/unbound/unbound.conf.d/101-intiop.conf
-
+cat magicneko/*.conf > /etc/unbound/unbound.conf.d/102-mnekoop.conf
+cat conf.op/*.conf > /etc/unbound/unbound.conf.d/103-intop.conf
